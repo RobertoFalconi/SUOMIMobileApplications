f8d11b6d8a8d9c8a1561cd2bf8d60e6ced3dea2d
Federico Guidi <federicoguidi196@gmail.com>
2018-09-01 12:07:24 +0200

    Test

--- MobileApplications/Resources/Resource.Designer.cs
+++ MobileApplications/Resources/Resource.Designer.cs
@@ -3213,6 +3213,9 @@ namespace MobileApplications
 			// aapt resource value: 0x7f030002
 			public const int ic_launcher_round = 2130903042;
 			
+			// aapt resource value: 0x7f030003
+			public const int penguin_icon = 2130903043;
+			
 			static Mipmap()
 			{
 				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
