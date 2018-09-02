eeabfedd50675e343cd0e3b6d1a40dabdefe633d
Federico Guidi <federicoguidi196@gmail.com>
2018-09-02 20:00:31 +0200

    Back button + ScrollView layout

--- MobileApplications/NewPageActivity.cs
+++ MobileApplications/NewPageActivity.cs
@@ -25,8 +25,20 @@ namespace MobileApplications
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
+            SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            SupportActionBar.SetDisplayShowHomeEnabled(true);
+
+            toolbar.SetNavigationIcon(Resource.Drawable.abc_ic_ab_back_material);
+
             Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 27, 49, 71));
 
+            //Window.AddFlags(WindowManagerFlags.TranslucentStatus);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (item.ItemId == Android.Resource.Id.Home) Finish();
+            return base.OnOptionsItemSelected(item);
         }
     }
 }
